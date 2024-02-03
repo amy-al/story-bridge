a6a71ff99a7a87d7604b2a9e40b4e512f493732f
Amy Li <amy.al12345@gmail.com>
2024-02-03 13:27:21 -0800

    new clean slate

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-import UserInfo from "@/components/UserInfo";
-
-export default function Home() {
-  return (
-    <div className="grid place-items-center h-screen -mt-24">
-      <UserInfo />
-    </div>
-  );
-}
